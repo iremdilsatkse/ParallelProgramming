d4660a3c532f5a71d478e612ac254fc7af629e5a
iremdilsatkse <147941220+iremdilsatkse@users.noreply.github.com>
2023-11-16 16:49:35 +0300

    Create pi_irem_dilsat_kose.py

---
+++ Week06/hw/pi_irem_dilsat_kose.py
@@ -0,0 +1,12 @@
+import random
+
+def next_pi() -> float:
+    total =0
+    inner =0
+    while True:
+        x = random.random()
+        y = random.random()
+        if(x**2 + y**2) <= 1:
+            inner += 1
+        total += 1
+        yield((inner/total)*4)
